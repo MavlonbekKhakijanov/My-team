fd0806315a2a901900bb3dbc7700e1a8cf76a87b
MavlonbekKhakijanov <khakijanovamor@gmail.com>
2024-04-09 17:52:57 +0300

    about man section done

---
+++ app.js
@@ -0,0 +1,6 @@
+// const icon = document.querySelectorAll(".plus-icon");
+// const overlay = document.querySelector("#overlay");
+
+// icon.addEventListener("click", () => {
+//   overlay.classList.toggle("hidden");
+// });
